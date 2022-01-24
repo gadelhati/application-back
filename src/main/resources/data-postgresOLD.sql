a8e694880f0a46d807e63e5f963c57c7b437e5ec
Gadelha <gadelha.ti@gmail.com>
2022-01-24 01:47:02 -0300

    popular

--- src/main/resources/data-postgresOLD.sql
+++ src/main/resources/data-postgresOLD.sql
@@ -1,8 +1,8 @@
-INSERT INTO chm.role(id, name) VALUES('7c12004d-e843-4e00-be40-01845ad75834', 'ROLE_USER');
-INSERT INTO chm.role(id, name) VALUES('52c57a80-4e3b-4a41-a864-58d0cea25b14', 'ROLE_MODERATOR');
-INSERT INTO chm.role(id, name) VALUES('8652ec73-0a53-433c-93be-420f1d90c681', 'ROLE_ADMIN');
-INSERT INTO chm.role(id, name) VALUES('f63b534f-154b-498f-b424-8469948d2f78', 'ROLE_RECTIFIER');
-INSERT INTO chm.role(id, name) VALUES('8cbfd8a2-db75-4bca-850b-d638a33701aa', 'ROLE_OPERATOR');
+INSERT INTO observation.role(id, name) VALUES('7c12004d-e843-4e00-be40-01845ad75834', 'ROLE_USER');
+INSERT INTO observation.role(id, name) VALUES('52c57a80-4e3b-4a41-a864-58d0cea25b14', 'ROLE_MODERATOR');
+INSERT INTO observation.role(id, name) VALUES('8652ec73-0a53-433c-93be-420f1d90c681', 'ROLE_ADMIN');
+INSERT INTO observation.role(id, name) VALUES('f63b534f-154b-498f-b424-8469948d2f78', 'ROLE_RECTIFIER');
+INSERT INTO observation.role(id, name) VALUES('8cbfd8a2-db75-4bca-850b-d638a33701aa', 'ROLE_OPERATOR');
 
---INSERT INTO chm.user(id, email, password, username) VALUES('5d40ea93-4965-4c9b-9ded-43a46839892e', 'marcelo@gmail.com', '$2a$10$DqzOBF0KUck60O3hRRQUMO4VHpabRHmhAkzAd5RXSTFIKK3jItHDu', 'marcelo');
---INSERT INTO chm.user_role(user_id, role_id) VALUES (5, ?);
+--INSERT INTO observation.user(id, email, password, username) VALUES('5d40ea93-4965-4c9b-9ded-43a46839892e', 'marcelo@gmail.com', '$2a$10$DqzOBF0KUck60O3hRRQUMO4VHpabRHmhAkzAd5RXSTFIKK3jItHDu', 'marcelo');
+--INSERT INTO observation.user_role(user_id, role_id) VALUES ('5d40ea93-4965-4c9b-9ded-43a46839892e', '8652ec73-0a53-433c-93be-420f1d90c681');
