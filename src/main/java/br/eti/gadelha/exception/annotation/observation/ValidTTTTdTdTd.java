6ade10c34fccaa76d7c7d712c82370af29dd1a05
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-02-02 14:56:22 -0300

    Tarefa #252: TdTdTd > TTT

---
+++ src/main/java/br/eti/gadelha/exception/annotation/observation/ValidTTTTdTdTd.java
@@ -0,0 +1,18 @@
+package br.eti.gadelha.exception.annotation.observation;
+
+import br.eti.gadelha.exception.validator.observation.ValidTTTTdTdTdValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = { ValidTTTTdTdTdValidator.class })
+@Documented
+public @interface ValidTTTTdTdTd {
+
+    String message() default "Verificar o valor de TdTdTd. O mesmo deve ser menor ou igual TTT";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+}
