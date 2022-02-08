bfd681ab28d0dca765e118651e3e647fb1168f75
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-02-08 14:20:23 -0300

    group validation: nhclcmchh

--- src/main/java/br/eti/gadelha/exception/validator/observation/ValidNCLCMCHhValidator.java
+++ src/main/java/br/eti/gadelha/exception/validator/observation/ValidNCLCMCHhValidator.java
@@ -16,10 +16,10 @@ public class ValidNCLCMCHhValidator implements ConstraintValidator<ValidNCLCMCHh
         if ( value == null && value.getNh() == null && value.getCl() == null && value.getCm() == null && value.getCh() == null && value.getH() == null ) {
             return false;
         } else if(value.getNh().equals("9")
-                && !value.getCl().equals("/") && Integer.parseInt(value.getCl()) > 0
-                || !value.getCm().equals("/") && Integer.parseInt(value.getCm()) > 0
-                && !value.getCh().equals("/") && Integer.parseInt(value.getCh()) > 0
-                && !value.getH().equals("9")) {
+                && (!value.getCl().equals("/") && Integer.parseInt(value.getCl()) >= 0
+                || !value.getCm().equals("/") && Integer.parseInt(value.getCm()) >= 0
+                || !value.getCh().equals("/") && Integer.parseInt(value.getCh()) >= 0
+                || !value.getH().equals("/") && Integer.parseInt(value.getH()) >= 0 )) {
             return false;
         } else {
             return true;
