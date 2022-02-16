6b311942053c7b76500967342569f85328247ba4
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-02-16 08:40:54 -0300

    user_role to user_roles

--- src/main/java/br/eti/gadelha/persistence/model/User.java
+++ src/main/java/br/eti/gadelha/persistence/model/User.java
@@ -47,7 +47,7 @@ public class User extends GenericEntity {
 	private OM om;
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(	name = "user_role",
+	@JoinTable(	name = "user_roles",
 				joinColumns = @JoinColumn(name = "user_id"), 
 				inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
