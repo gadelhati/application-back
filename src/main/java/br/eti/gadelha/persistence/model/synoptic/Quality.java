a116aaec36d02a38659c2f856b28e507ef8bdb27
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-07-21 08:50:20 -0300

    clean

--- src/main/java/br/eti/gadelha/persistence/model/synoptic/Quality.java
+++ src/main/java/br/eti/gadelha/persistence/model/synoptic/Quality.java
@@ -1,14 +1,25 @@
 package br.eti.gadelha.persistence.model.synoptic;
 
+import br.eti.gadelha.persistence.model.GenericEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+/**
+ * @author	Marcelo Ribeiro Gadelha
+ * @mail	marcelo.gadelha@marinha.mil.br
+ * @link	www.gadelha.eti.br
+ **/
 
 @Audited @Entity @Table @Data @AllArgsConstructor @NoArgsConstructor @EqualsAndHashCode(callSuper = false)
-public class Quality {
+public class Quality extends GenericEntity {
+
+    private String io;
+    private String ie;
+    private String iq;
+    private String criticado;
 }
