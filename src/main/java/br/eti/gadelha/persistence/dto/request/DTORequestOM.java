9ed7af75e34798a13fd5ed011bc577df0ec0975b
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-09 16:08:19 -0300

    clean test validation

--- src/main/java/br/eti/gadelha/persistence/dto/request/DTORequestOM.java
+++ src/main/java/br/eti/gadelha/persistence/dto/request/DTORequestOM.java
@@ -1,6 +1,5 @@
 package br.eti.gadelha.persistence.dto.request;
 
-import br.eti.gadelha.exception.annotation.TesteOM;
 import br.eti.gadelha.persistence.model.OM;
 import lombok.Getter;
 
@@ -10,7 +9,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class DTORequestOM {
 
-    @NotNull(message = "{name.not.null}") @NotBlank(message = "{name.not.blank}") @TesteOM //@UniqueNameOM
+    @NotNull(message = "{name.not.null}") @NotBlank(message = "{name.not.blank}")  //@UniqueNameOM
     private String name;
 
     public OM toObject(){
