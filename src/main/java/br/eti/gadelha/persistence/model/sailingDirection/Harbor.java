a34eccf1f97e1a32989e24119dd0d27ee9b940e3
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-15 16:24:40 -0300

    harbor

--- src/main/java/br/eti/gadelha/persistence/model/sailingDirection/Harbor.java
+++ src/main/java/br/eti/gadelha/persistence/model/sailingDirection/Harbor.java
@@ -2,6 +2,7 @@ package br.eti.gadelha.persistence.model.sailingDirection;
 
 import br.eti.gadelha.persistence.model.GenericEntity;
 import br.eti.gadelha.persistence.model.unity.Institution;
+import br.eti.gadelha.persistence.model.unity.Station;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,6 +15,9 @@ import javax.persistence.*;
 public class Harbor extends GenericEntity {
     private String name;
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
-    @JoinColumn(name = "institution"/*, nullable = true*/)
+    @JoinColumn(name = "institution", nullable = false)
     private Institution institution;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "station", nullable = false)
+    private Station station;
 }
