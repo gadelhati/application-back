8540f2eeba7a4db84718daea7e1813801791ab15
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-18 10:49:29 -0300

    refactoring

--- src/main/java/br/eti/gadelha/controller/ControllerSynopticObservation.java
+++ src/main/java/br/eti/gadelha/controller/ControllerSynopticObservation.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/observation")
+@RequestMapping("/synopticObservation")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ControllerSynopticObservation {
 
