44a75e22e67fc81ddabe658587ac3cd6f16c23f5
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-24 10:59:25 -0300

    clean

--- src/main/java/br/eti/gadelha/controller/ControllerSynopticObservation.java
+++ src/main/java/br/eti/gadelha/controller/ControllerSynopticObservation.java
@@ -12,7 +12,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
