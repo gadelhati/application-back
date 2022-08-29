2550b0b9527ea754f832d213ff1e930966e9fe35
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-29 13:48:29 -0300

    remove validations

--- src/main/java/br/eti/gadelha/persistence/dto/request/DTORequestSynopticObservation.java
+++ src/main/java/br/eti/gadelha/persistence/dto/request/DTORequestSynopticObservation.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Getter @AllArgsConstructor @NoArgsConstructor @JsonIgnoreProperties(ignoreUnknown = true)
-@ValidNhCLCM @ValidTTTTdTdTd @ValidAppp @ValidCLCM @ValidCMCH @ValidNCLCMCH @ValidNCLCMCHh @ValidNddff @ValidNh @ValidNhCLCMCHh @ValidNhCLCMh @ValidNNh @ValidNW2 @ValidTdTdTd @ValidTnTnTn @ValidTTT @ValidTxTxTx @ValidVVh @ValidVVWW @ValidW1W2
+/*@ValidNhCLCM*/ @ValidTTTTdTdTd @ValidAppp @ValidCLCM @ValidCMCH @ValidNCLCMCH @ValidNCLCMCHh @ValidNddff @ValidNh @ValidNhCLCMCHh @ValidNhCLCMh @ValidNNh /*@ValidNW2*/ @ValidTdTdTd @ValidTnTnTn @ValidTTT @ValidTxTxTx @ValidVVh @ValidVVWW @ValidW1W2
 public class DTORequestSynopticObservation {
 
 //    private Point coordinates;
