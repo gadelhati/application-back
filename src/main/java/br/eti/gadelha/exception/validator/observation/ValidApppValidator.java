149e40450cdb9dd9b5f7e1d9c3be6ada321b286c
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-09-26 14:04:38 -0300

    global validation

--- src/main/java/br/eti/gadelha/exception/validator/observation/ValidApppValidator.java
+++ src/main/java/br/eti/gadelha/exception/validator/observation/ValidApppValidator.java
@@ -6,6 +6,9 @@ import br.eti.gadelha.persistence.dto.request.DTORequestSynopticObservation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static br.eti.gadelha.exception.validator.GlobalValidation.isValidWithName;
+import static br.eti.gadelha.exception.validator.GlobalValidation.isValidWithNumber;
+
 public class ValidApppValidator implements ConstraintValidator<ValidAppp, DTORequestSynopticObservation> {
 
     @Override
@@ -16,19 +19,32 @@ public class ValidApppValidator implements ConstraintValidator<ValidAppp, DTOReq
         if ( value == null ) {
             return false;
         } else if (value.getA() != null) {
-            if (value.getPpp() != null && !value.getPpp().isEmpty() && value.getA().equals("/") && !value.getPpp().equals("///") && Integer.parseInt(value.getPpp()) >= 0) {
-                return false;
-            } else if (!value.getA().isEmpty() && !value.getA().equals("/") && Integer.parseInt(value.getA()) >= 1 && Integer.parseInt(value.getA()) <= 8 && value.getPpp().equals("///")) {
-                return false;
-            } else if (!value.getA().isEmpty() && !value.getPpp().isEmpty() && !value.getA().equals("/") && Integer.parseInt(value.getA()) == 4 && !value.getPpp().equals("///") && Integer.parseInt(value.getPpp()) > 0) {
-                return false;
-            } else if (value.getPpp() != null && !value.getA().isEmpty() && !value.getPpp().isEmpty() && !value.getA().equals("/") && Integer.parseInt(value.getA()) >= 1 && Integer.parseInt(value.getA()) <= 3 && value.getPpp().equals("000")) {
-                return false;
-            } else if (!value.getA().isEmpty() && !value.getA().equals("/") && Integer.parseInt(value.getA()) >= 6 && Integer.parseInt(value.getA()) <= 8 && value.getPpp().equals("000")) {
-                return false;
+            if (isValidWithName(value.getPpp())) {
+                if (isValidWithNumber(value.getPpp())) {
+                    if (value.getA().equals("/") && !value.getPpp().equals("///") && Integer.parseInt(value.getPpp()) >= 0) {
+                        return false;
+                    }
+                }
+                if (isValidWithNumber(value.getA())) {
+                    if (!value.getA().equals("/")) {
+                        if (Integer.parseInt(value.getA()) >= 1 && Integer.parseInt(value.getA()) <= 8 && value.getPpp().equals("///")) {
+                            return false;
+                        }
+                        if (Integer.parseInt(value.getA()) == 4 && !value.getPpp().equals("///") && Integer.parseInt(value.getPpp()) > 0) {
+                            return false;
+                        }
+                        if (Integer.parseInt(value.getA()) >= 1 && Integer.parseInt(value.getA()) <= 3 && value.getPpp().equals("000")) {
+                            return false;
+                        }
+                        if (Integer.parseInt(value.getA()) >= 6 && Integer.parseInt(value.getA()) <= 8 && value.getPpp().equals("000")) {
+                            return false;
+                        }
+                    }
+                }
             } else {
                 return true;
             }
+            return true;
         } else {
                 return true;
         }
