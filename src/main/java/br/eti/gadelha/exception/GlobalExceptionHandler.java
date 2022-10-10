5bbea8e94c3ea245dc6f2014ba57036c8b68e6f7
Gadelha TI <gadelha.ti@gmail.com>
2022-10-10 10:08:22 -0300

    cleaning up

--- src/main/java/br/eti/gadelha/exception/GlobalExceptionHandler.java
+++ src/main/java/br/eti/gadelha/exception/GlobalExceptionHandler.java
@@ -1,10 +1,5 @@
 package br.eti.gadelha.exception;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +7,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,54 +16,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders httpHeaders, HttpStatus httpStatus, WebRequest webRequest) {
         ErrorResponse errorResponse = new ErrorResponse(httpStatus, exception.getStackTrace().toString(), exception.getAllErrors().toString());
         for(ObjectError objectError : exception.getBindingResult().getAllErrors()) {
-//            if (((FieldError) objectError).getField() == null) {
-//                errorResponse.addValidationError("Group", "objectError.getDefaultMessage()");
-//            } else {
                 errorResponse.addValidationError(((FieldError) objectError).getField(), objectError.getDefaultMessage());
-//            }
         }
-//        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-//            errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
-//        }
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
-//    @ExceptionHandler({ AccessDeniedException.class })
-//    public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
-////        return new ResponseEntity<Object>("Access denied message here", new HttpHeaders(), HttpStatus.FORBIDDEN);
-//        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN, "exception.getStackTrace().toString()", "exception.getAllErrors().toString()");
-////        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-//            errorResponse.addValidationError("fieldError.getField()", "fieldError.getDefaultMessage()");
-////        }
-//        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-//    }
-//    @ExceptionHandler({UnauthorizedRequestException.class})
-//    protected ResponseEntity<Object> handleUnauthorizedRequestException(UnauthorizedRequestException e, WebRequest request) {
-//        ErrorResource error = new ErrorResource("403", e.getMessage());
-//        return new ResponseEntity<Object>(error, HttpStatus.FORBIDDEN);
-//    }
-//    @ExceptionHandler(ItemNotFoundException.class)
-//    protected ResponseEntity<Object> handleEntityNotFound(ItemNotFoundException ex, HttpServletRequest request) {
-////        log.debug("A REST API error occurred during web call [{}:{}].", request.getMethod(), request.getRequestURI() ,ex);
-//        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, "The requested resource was not found.", ex.getMessage());
-//        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-//    }
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<Object> handle(Exception exception, HttpServletRequest request, HttpServletResponse response) {
-//        if (exception instanceof NullPointerException) {
-//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//        }
-//        if (exception instanceof MethodArgumentNotValidException) {
-//            ErrorResponse errorResponse = new ErrorResponse(httpStatus, exception.getStackTrace().toString(), exception.getAllErrors().toString());
-//            for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-//                errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
-//            }
-//            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//        }
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//    }
-//    @ExceptionHandler(AccessDeniedException.class)
-//    @ResponseStatus(HttpStatus.FORBIDDEN)
-//    public ResponseEntity<Object> handleAccessDeniedException(Exception e, WebRequest request) {
-//        return new ResponseEntity<Object>("Acesso negado!", HttpStatus.FORBIDDEN);
-//    }
 }
