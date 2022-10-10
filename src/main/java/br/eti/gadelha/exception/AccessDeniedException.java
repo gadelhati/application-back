82684d578a28ff15527bf65afd135351187c0a82
Gadelha TI <gadelha.ti@gmail.com>
2022-10-10 10:08:03 -0300

    remove unused exception classes

--- src/main/java/br/eti/gadelha/exception/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package br.eti.gadelha.exception;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/eti/gadelha/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-//package br.eti.gadelha.exception;
-//
-//public class ResourceNotFoundException extends RuntimeException {
-//    public ResourceNotFoundException(String message) {
-//        super(message);
-//    }
-//}
