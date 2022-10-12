cb098dabcc662798fc604aafcf617dbc02433341
Gadelha <gadelha.ti@gmail.com>
2022-10-12 10:09:08 -0300

    merge and rebase to cleaning up

--- src/main/java/br/eti/gadelha/exception/NoSuchElementFoundException.java
+++
@@ -1,7 +0,0 @@
-//package br.eti.gadelha.exception;
-//
-//public class NoSuchElementFoundException extends RuntimeException {
-//    public NoSuchElementFoundException(String message) {
-//        super(message);
-//    }
-//}
