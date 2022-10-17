3895db46d72fa4c9dc7937e32ca75b5eff8d5cec
Gadelha TI <gadelha.ti@gmail.com>
2022-10-17 12:37:29 -0300

    refactoring ValidNW2Validator

--- src/main/java/br/eti/gadelha/exception/validator/synopticObservation/ValidNW2Validator.java
+++ src/main/java/br/eti/gadelha/exception/validator/synopticObservation/ValidNW2Validator.java
@@ -6,6 +6,8 @@ import br.eti.gadelha.persistence.payload.request.DTORequestSynopticObservation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static br.eti.gadelha.exception.validator.GlobalValidation.isValidWithNumber;
+
 public class ValidNW2Validator implements ConstraintValidator<ValidNW2, DTORequestSynopticObservation> {
 
     @Override
@@ -13,16 +15,10 @@ public class ValidNW2Validator implements ConstraintValidator<ValidNW2, DTOReque
     }
     @Override
     public boolean isValid(DTORequestSynopticObservation value, ConstraintValidatorContext context) {
-        if ( value == null ) {
-            return false;
-        } else if( value.getN() != null
-                && !value.getN().isEmpty() && !value.getN().equals("/") && Integer.parseInt(value.getN()) >= 5 && Integer.parseInt(value.getN()) <= 8
-                && !value.getW1w2().isEmpty() && !value.getW1w2().substring(1).equals("/") && Integer.parseInt(value.getW1w2().substring(1)) == 0 ) {
-            return false;
-        } else if( value.getN() != null
-                && !value.getN().isEmpty() && !value.getN().equals("/") && Integer.parseInt(value.getN()) >= 0 && Integer.parseInt(value.getN()) <= 4
-                && !value.getW1w2().isEmpty() && !value.getW1w2().substring(1).equals("/") && Integer.parseInt(value.getW1w2().substring(1)) == 2 ) {
-            return false;
+        if(isValidWithNumber(value.getN()) &&
+                value.getW1w2().substring(1).equals("2") && Integer.parseInt(value.getN()) >= 0 && Integer.parseInt(value.getN()) <= 4 ||
+                value.getW1w2().substring(1).equals("0") && Integer.parseInt(value.getN()) >= 5 && Integer.parseInt(value.getN()) <= 8) {
+           return false;
         } else {
             return true;
         }
