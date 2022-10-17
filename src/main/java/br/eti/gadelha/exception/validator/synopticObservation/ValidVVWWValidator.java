89e21a386a0b0bc90fef34964f508098c580e608
Gadelha TI <gadelha.ti@gmail.com>
2022-10-17 11:01:48 -0300

    refactoring ValidVVWWValidator

--- src/main/java/br/eti/gadelha/exception/validator/synopticObservation/ValidVVWWValidator.java
+++ src/main/java/br/eti/gadelha/exception/validator/synopticObservation/ValidVVWWValidator.java
@@ -6,6 +6,8 @@ import br.eti.gadelha.persistence.payload.request.DTORequestSynopticObservation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import static br.eti.gadelha.exception.validator.GlobalValidation.isValidWithNumber;
+
 public class ValidVVWWValidator implements ConstraintValidator<ValidVVWW, DTORequestSynopticObservation> {
 
     @Override
@@ -13,25 +15,23 @@ public class ValidVVWWValidator implements ConstraintValidator<ValidVVWW, DTOReq
     }
     @Override
     public boolean isValid(DTORequestSynopticObservation value, ConstraintValidatorContext context) {
-        if ( value == null ) {
-            return false;
-        } else if( value.getWw() != null && value.getVv() != null
-                && !value.getWw().isEmpty() && !value.getVv().isEmpty() && Integer.parseInt(value.getVv()) < Integer.parseInt(value.getWw())) {
-            return false;
-        } else if( value.getWw() != null && value.getVv() != null
-                && !value.getWw().isEmpty() && !value.getVv().isEmpty() && Integer.parseInt(value.getVv()) >= 94 && Integer.parseInt(value.getVv()) <= 99
-                && Integer.parseInt(value.getWw()) >= 40 && Integer.parseInt(value.getWw()) <= 49) {
-            return false;
-        } else if( value.getWw() != null && value.getVv() != null
-                && !value.getWw().isEmpty() && !value.getVv().isEmpty() && Integer.parseInt(value.getVv()) >= 90 && Integer.parseInt(value.getVv()) <= 93
-                && value.getWw().equals("10")) {
-            return false;
-        } else if( value.getWw() != null && value.getVv() != null
-                && !value.getWw().isEmpty() && !value.getVv().isEmpty() && Integer.parseInt(value.getVv()) >= 90 && Integer.parseInt(value.getVv()) <= 96
-                && Integer.parseInt(value.getWw()) >= 10 && Integer.parseInt(value.getWw()) <= 16) {
-            return false;
+        if(isValidWithNumber(value.getWw()) && isValidWithNumber(value.getWw())) {
+            if (Integer.parseInt(value.getVv()) < Integer.parseInt(value.getWw())) {
+                return false;
+            } else if (Integer.parseInt(value.getVv()) >= 94 && Integer.parseInt(value.getVv()) <= 99 &&
+                    Integer.parseInt(value.getWw()) >= 40 && Integer.parseInt(value.getWw()) <= 49) {
+                return false;
+            } else if (Integer.parseInt(value.getVv()) >= 90 && Integer.parseInt(value.getVv()) <= 93 &&
+                    value.getWw().equals("10")) {
+                return false;
+            } else if (Integer.parseInt(value.getVv()) >= 90 && Integer.parseInt(value.getVv()) <= 96 &&
+                    Integer.parseInt(value.getWw()) >= 10 && Integer.parseInt(value.getWw()) <= 16) {
+                return false;
+            } else {
+                return true;
+            }
         } else {
-            return true;
+            return false;
         }
     }
 }
